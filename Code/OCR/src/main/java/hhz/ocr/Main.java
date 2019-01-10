cf7ed759e2f756a5c44bbea7c1c3a2a808710994
Valerij <Valerij@DESKTOP-RSRJ8BL>
2019-01-10 22:08:37 +0100

    Add new project OCR. Productkey of this service is limited for 7 day (trial period, created 09.01.19) Todo: To check services for better results e.g. https://docs.microsoft.com/de-de/azure/cognitive-services/Computer-vision/QuickStarts/CSharp-analyze

---
+++ Code/OCR/src/main/java/hhz/ocr/Main.java
@@ -0,0 +1,88 @@
+package hhz.ocr;
+
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Valerij
+ */
+public class Main {
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    // Replace <Subscription Key> with your valid subscription key.
+    private static final String subscriptionKey = "136f11d104c04220be0c80630c99cda2";
+
+    // You must use the same Azure region in your REST API method as you used to
+    // get your subscription keys. For example, if you got your subscription keys
+    // from the West US region, replace "westcentralus" in the URL
+    // below with "westus".
+    //
+    // Free trial subscription keys are generated in the "westus" region.
+    // If you use a free trial subscription key, you shouldn't need to change
+    // this region.
+    private static final String uriBase =
+  "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr";
+//mit dem unterenn service werden bessere resultate erzielt, todo product key 
+//"https://westus.api.cognitive.microsoft.com/vision/v2.0/recognizeText";
+    private static final String imageToAnalyze =
+  "https://consiliarius.files.wordpress.com/2015/03/img_20150328_131815.jpg"; 
+//"http://www.pi-news.net/wp-content/uploads/2011/08/Greifswald3.jpg";          
+//"https://www.supermarktblog.com/wp-content/uploads/2014/06/nettopreis02.jpg";
+//"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/" +
+//"Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            URIBuilder uriBuilder = new URIBuilder(uriBase);
+
+            uriBuilder.setParameter("language", "de");
+            uriBuilder.setParameter("detectOrientation", "true");
+
+            // Request parameters.
+            URI uri = uriBuilder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Call the REST API method and get the response entity.
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("REST Response:\n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
+}
