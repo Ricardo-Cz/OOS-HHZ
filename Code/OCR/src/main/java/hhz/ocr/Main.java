01d9eadd9a45e97be02a813293aa76df78767654
Valerij <Valerij@DESKTOP-RSRJ8BL>
2019-01-14 18:12:57 +0100

    Add new api for text recognition that better as last one.

--- Code/OCR/src/main/java/hhz/ocr/Main.java
+++ Code/OCR/src/main/java/hhz/ocr/Main.java
@@ -1,32 +1,24 @@
 package hhz.ocr;
-
 import java.net.URI;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.apache.http.Header;
 import org.json.JSONObject;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Valerij
- */
 public class Main {
     // **********************************************
     // *** Update or verify the following values. ***
     // **********************************************
 
     // Replace <Subscription Key> with your valid subscription key.
-    private static final String subscriptionKey = "136f11d104c04220be0c80630c99cda2";
+    private static final String subscriptionKey = "c4e7960dcdbb4b55a0577631903670b6";
 
     // You must use the same Azure region in your REST API method as you used to
     // get your subscription keys. For example, if you got your subscription keys
@@ -37,27 +29,28 @@ public class Main {
     // If you use a free trial subscription key, you shouldn't need to change
     // this region.
     private static final String uriBase =
-  "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr";
-//mit dem unterenn service werden bessere resultate erzielt, todo product key 
-//"https://westus.api.cognitive.microsoft.com/vision/v2.0/recognizeText";
+          "https://westeurope.api.cognitive.microsoft.com/vision/v2.0/recognizeText";
+        //"https://westus.api.cognitive.microsoft.com/vision/v2.0/recognizeText";
+
     private static final String imageToAnalyze =
-  "https://consiliarius.files.wordpress.com/2015/03/img_20150328_131815.jpg"; 
-//"http://www.pi-news.net/wp-content/uploads/2011/08/Greifswald3.jpg";          
-//"https://www.supermarktblog.com/wp-content/uploads/2014/06/nettopreis02.jpg";
-//"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/" +
-//"Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png";
+    "https://consiliarius.files.wordpress.com/2015/03/img_20150328_131815.jpg";        
 
     public static void main(String[] args) {
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        CloseableHttpClient httpTextClient = HttpClientBuilder.create().build();
+        CloseableHttpClient httpResultClient = HttpClientBuilder.create().build();;
 
         try {
-            URIBuilder uriBuilder = new URIBuilder(uriBase);
+            // This operation requires two REST API calls. One to submit the image
+            // for processing, the other to retrieve the text found in the image.
 
-            uriBuilder.setParameter("language", "de");
-            uriBuilder.setParameter("detectOrientation", "true");
+            URIBuilder builder = new URIBuilder(uriBase);
 
-            // Request parameters.
-            URI uri = uriBuilder.build();
+            // Request parameter.
+            builder.setParameter("mode", "Printed");
+           
+          
+            // Prepare the URI for the REST API method.
+            URI uri = builder.build();
             HttpPost request = new HttpPost(uri);
 
             // Request headers.
@@ -69,19 +62,70 @@ public class Main {
                     new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
             request.setEntity(requestEntity);
 
-            // Call the REST API method and get the response entity.
-            HttpResponse response = httpClient.execute(request);
-            HttpEntity entity = response.getEntity();
+            // Two REST API methods are required to extract handwritten text.
+            // One method to submit the image for processing, the other method
+            // to retrieve the text found in the image.
 
-            if (entity != null) {
-                // Format and display the JSON response.
+            // Call the first REST API method to detect the text.
+            HttpResponse response = httpTextClient.execute(request);
+
+            // Check for success.
+            if (response.getStatusLine().getStatusCode() != 202) {
+                // Format and display the JSON error message.
+                HttpEntity entity = response.getEntity();
                 String jsonString = EntityUtils.toString(entity);
                 JSONObject json = new JSONObject(jsonString);
-                System.out.println("REST Response:\n");
+                System.out.println("Error:\n");
+                System.out.println(json.toString(2));
+                return;
+            }
+
+            // Store the URI of the second REST API method.
+            // This URI is where you can get the results of the first REST API method.
+            String operationLocation = null;
+
+            // The 'Operation-Location' response header value contains the URI for
+            // the second REST API method.
+            Header[] responseHeaders = response.getAllHeaders();
+            for (Header header : responseHeaders) {
+                if (header.getName().equals("Operation-Location")) {
+                    operationLocation = header.getValue();
+                    break;
+                }
+            }
+
+            if (operationLocation == null) {
+                System.out.println("\nError retrieving Operation-Location.\nExiting.");
+                System.exit(1);
+            }
+
+            // If the first REST API method completes successfully, the second
+            // REST API method retrieves the text written in the image.
+            //
+            // Note: The response may not be immediately available. Handwriting
+            // recognition is an asynchronous operation that can take a variable
+            // amount of time depending on the length of the handwritten text.
+            // You may need to wait or retry this operation.
+
+            System.out.println("\nHandwritten text submitted.\n" +
+                    "Waiting 10 seconds to retrieve the recognized text.\n");
+            Thread.sleep(10000);
+
+            // Call the second REST API method and get the response.
+            HttpGet resultRequest = new HttpGet(operationLocation);
+            resultRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            HttpResponse resultResponse = httpResultClient.execute(resultRequest);
+            HttpEntity responseEntity = resultResponse.getEntity();
+
+            if (responseEntity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(responseEntity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Text recognition result response: \n");
                 System.out.println(json.toString(2));
             }
         } catch (Exception e) {
-            // Display error message.
             System.out.println(e.getMessage());
         }
     }
